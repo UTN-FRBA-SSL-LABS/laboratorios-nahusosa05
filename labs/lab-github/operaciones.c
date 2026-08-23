c15839b63fdd15e4caeae14873a327b9c8202405
Nahuel Sosa <nasosa@frba.utn.edu.ar>
2026-08-23 14:42:21 -0300

    saco comentario TODO

--- labs/lab-github/operaciones.c
+++ labs/lab-github/operaciones.c
@@ -15,7 +15,7 @@ int restar(int a, int b) {
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
 /* multiplicar() recibe 2 ints y devuelve su producto. */
 int multiplicar(int a, int b) {
-    return a*b; /* TODO: implementar */
+    return a*b; 
 }
 
 /* ── esPar ────────────────────────────────────────────────────────────────── */
