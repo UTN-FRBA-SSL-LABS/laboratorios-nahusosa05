27ac85343099de68db386f3d5dafa94e32a81ac0
Nahuel Sosa <nasosa@frba.utn.edu.ar>
2026-08-23 14:38:24 -0300

    agrego comentario explicativo en multiplicar

--- labs/lab-github/operaciones.c
+++ labs/lab-github/operaciones.c
@@ -13,7 +13,7 @@ int restar(int a, int b) {
 }
 
 /* ── multiplicar — implementar en feature/mi-funcion ─────────────────────── */
-
+/* multiplicar() recibe 2 ints y devuelve su producto. */
 int multiplicar(int a, int b) {
     return a*b; /* TODO: implementar */
 }
